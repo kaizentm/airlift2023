537e41b0910a0dac8324469ed15df00f972a795f
polatengin <enpolat@microsoft.com>
2023-10-12 22:42:20 +0000

    adding tests

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,12 @@ resource_group_name="airlift_prod"
 
 az group create --name "rg-${resource_group_name}" --location "${location}" --tags "TeamName=DevOps"
 
-az deployment group create --name "${resource_group_name}" --resource-group "rg-${resource_group_name}" --template-file "./bicep/main.bicep" --parameters "./bicep/main.prod.bicepparam"
+output=$(az deployment group create --name "${resource_group_name}" --resource-group "rg-${resource_group_name}" --template-file "./bicep/main.bicep" --parameters "./bicep/main.${environmentName}.bicepparam")
+
+echo $output
+
+pushd ./bicep
+
+pwsh -Command "./End_To_End_Test.ps1"
+
+popd
